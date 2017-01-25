16c0f527bd23ce29c373d0f6692f9a79f9971c9a
Daffyneko <daffyneko@gmail.com>
2017-01-25 18:25:12 +0100

    Create ecole.h

---
+++ ecole.h
@@ -0,0 +1,20 @@
+#include <stdio.>
+
+#include "classe.h"
+
+#ifndef ECOLE_H
+#define ECOLE_H
+
+typedef struct Ecole
+{
+	char nomEcole[MAX]; // Nom de l'école
+	char nomDirecteur[MAX]; // Nom du directeur de lécole
+	int nbEleveTotal; // Nombre total d'élève dans l'école
+	int nbEleveNiveau[5]; // Nombre d'élève par niveau. Chaque case représente un niveau
+	int nbClasse; // Nombre de classe totale dans l'école
+
+	struct Classe classe[25]; // Appel de la fonction classe pour saisir une classe dans l'école
+//          ******  Utilisation d'un simple tableau ou bien d'une table de hachage ?    *******
+} Ecole_t;
+
+#endif
