075e12e443337a46aa7be91590916c2bab0678ef
Daffyneko <daffyneko@gmail.com>
2017-01-25 16:35:16 +0100

    Add files via upload

---
+++ eleve.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "eleve.h"
+
+
+
+void SaisirEleve(Eleve_t *E){
+
+}
+void AfficherEleve(Eleve_t E){
+
+}
---
+++ eleve.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+#ifndef ELEVE_H
+#define ELEVE_H
+
+#define MAX 30
+
+typedef struct Eleve{
+    char nom[MAX];
+    char prenom[MAX];
+    char sexe;
+    struct tm dateNaissance; // Date de naissance. Utilisation de la structure tm. Ajout de la biblio <time.h>
+//	struct Parent renseignement; // Renseignement sur les parents numero nom et adresse
+//	struct Bulletin note; // Bulletins de notes. notes matières retard abscences...
+
+    struct Eleve *suivant; // Utilisation de liste chainée pour avoir une liste d'élève
+}Eleve_t;
+
+void SaisirEleve(Eleve_t *E);
+void AfficherEleve(Eleve_t E);
+
+#endif // ELEVE_H
