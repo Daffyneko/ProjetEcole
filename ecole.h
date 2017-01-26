f58876a98219bd7ea4398f0caaf6e7fde13ca3a7
Daffyneko <daffyneko@gmail.com>
2017-01-26 12:25:17 +0100

    Update ecole.h

--- ecole.h
+++ ecole.h
@@ -1,23 +1,25 @@
-#include <stdio.>
-
-#include "classe.h"
+#include <stdio.h>
+#include <time.h>
 
 #ifndef ECOLE_H
 #define ECOLE_H
 
-typedef struct Ecole
-{
+#include "classe.h"
+# define MAX 30
+# define CHAINE 150
+# define MAXELEVE 25
+
+typedef struct Ecole{
 	char nomEcole[MAX]; // Nom de l'école
 	char nomDirecteur[MAX]; // Nom du directeur de lécole
 	int nbEleveTotal; // Nombre total d'élève dans l'école
 	int nbEleveNiveau[5]; // Nombre d'élève par niveau. Chaque case représente un niveau
 	int nbClasse; // Nombre de classe totale dans l'école
 
-	struct Classe classe[25]; // Appel de la fonction classe pour saisir une classe dans l'école
+	struct Classe TabClasse[4];
+
+//	struct Classe *classe[5]; // Appel de la fonction classe pour saisir une classe dans l'école
 //          ******  Utilisation d'un simple tableau ou bien d'une table de hachage ?    *******
 } Ecole_t;
-
-void SaisirEcole(Ecole_t *E);
-void AfficherEcole(Ecole_t E);
 
 #endif //ECOLE_H
