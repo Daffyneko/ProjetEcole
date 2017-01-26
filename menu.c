25f777cf657b91c06afd0d700c81f2868513e3a9
Daffyneko <daffyneko@gmail.com>
2017-01-26 17:33:36 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -5,16 +5,20 @@
 
 void MenuEleve(Ecole_t E){
     int choix=-1;
+    char recherche[MAX];//le mot à rechercher
+    int trouve, modifie;
+    int i, j;
     Eleve_t eleve;
     do{
         printf("\n*****Gestion des élèves*****\n");
-        printf("\nQue voulez-vous faire ?\n1 - Ajouter un élève\n2 - Afficher les élèves\n3 - Supprimer un élève\n0 - Retourner au menu précédent\n");
+        printf("\nQue voulez-vous faire ?\n1 - Ajouter un élève\n2 - Afficher les élèves\n3 - Modifier un élève\n4 - Supprimer un élève\n0 - Retourner au menu précédent\n");
         scanf("%d",&choix);
         switch(choix){
             default : break;
             case 0 : break;
             case 1 :
                 SaisirEleve(&eleve);
+                E.nbEleveTotal++;
                 printf("\n\nVous avez saisi les informations suivantes : ");
                 AfficherEleve(eleve);
                 break;
@@ -23,6 +27,23 @@ void MenuEleve(Ecole_t E){
                 AfficherEleve(eleve);//fonction temporaire, à remplacer par affichertabeleve
                 break;
             case 3 :
+                printf("\nVeuillez entrer le nom de l'élève à modifier : ");
+                fscanf(stdin,"%s",recherche);
+                modifie=0;
+                for(i=0;i<E.nbClasse;i++){
+                    for(j=0;j<E.TabClasse[i].nbEleveClasse;j++){
+                        trouve=Recherche(E.TabClasse[i].TabEleve[j].nom, recherche);
+                        if(trouve==0){
+                            ModifierEleve(&E.TabClasse[i].TabEleve[j]);
+                            modifie=1;
+                        }
+                    }
+                }
+                if(modifie!=1){
+                    printf("\n***L'élève n'a pas été trouvé***\n");
+                }
+                break;
+            case 4 :
                 printf("\n***Suppression d'un élève***\n");
                 break;
         }
@@ -30,6 +51,9 @@ void MenuEleve(Ecole_t E){
 }
 void MenuClasse(Ecole_t E){
     int choix=-1;
+    int i, trouve, modifie, numero;
+    char recherche;
+    Classe_t C;
     do{
         printf("\n*****Gestion des classes*****\n");
         printf("\nQue voulez-vous faire ?\n1 - Ajouter une classe\n2 - Modifier une classe\n3 - Afficher une classe\n0 - Retourner au menu précédent\n");
@@ -39,12 +63,29 @@ void MenuClasse(Ecole_t E){
             case 0 : break;
             case 1 :
                 printf("\n\n*****Saisie d'une classe*****\n");
+                SaisirClasse(&C);
                 break;
             case 2 :
-                printf("\n\n*****Modification d'une classe*****\n");
+                printf("\nVeuillez entrer le nom de la classe à modifier : ");
+                fscanf(stdin,"%s",recherche);
+                getchar();
+                printf("\nVeuillez entrer le numéro de la classe à modifier : ");
+                scanf("%d", &numero);
+                modifie=0;
+                for(i=0;i<E.nbClasse;i++){
+                    trouve=Recherche(E.TabClasse[i].niveau, recherche);
+                    if(trouve==0 && E.TabClasse[i].numClasse==numero){
+                        ModifierClasse(&E.TabClasse[i]);
+                        modifie=1;
+                        }
+                }
+                if(modifie!=1){
+                    printf("\n***La classe n'a pas été trouvée***\n");
+                }
                 break;
             case 3 :
                 printf("\n\n*****Affichage d'une classe*****\n");
+                AfficherClasse(C);
                 break;
         }
     }while(choix!=0);
@@ -60,12 +101,14 @@ void MenuEcole(Ecole_t E){
             case 0 : break;
             case 1 :
                 printf("\n\n*****Saisie des informations de l'école*****\n");
+                SaisirEcole(&E);
                 break;
             case 2 :
                 printf("\n\n*****Modification des informations de l'école*****\n");
                 break;
             case 3 :
                 printf("\n\n*****Affichage de l'école*****\n");
+                AfficherEcole(E);
                 break;
         }
     }while(choix!=0);
