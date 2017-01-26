dbb01ab4fce9e8b14fea2d7f396c11d13e13b9db
Daffyneko <daffyneko@gmail.com>
2017-01-26 12:24:51 +0100

    Update classe.h

--- classe.h
+++ classe.h
@@ -1,25 +1,23 @@
 #include <stdio.h>
-
-#include "eleve.h"
+#include <time.h>
 
 #ifndef CLASSE_H
 #define CLASSE_H
 
-#define MAX 30
-#define CHAINE 150
+#include "eleve.h"
+# define MAX 30
+# define CHAINE 150
+# define MAXELEVE 25
 
-typedef struct Classe
-{
+typedef struct Classe{
 	char niveau[4]; // Niveau de la classe CP CE1...
 	char nomEnseignant[MAX]; // Nom du prof s'occupant de la classe
 	int numClasse; // Numero de la classe dans le cas ou il y aurait plusieurs CP ou autres classes...
 	int nbEleveClasse; // Nombre d'élève par classe
-	struct Eleve eleveClasse; // Appel de la structure Eleve pour saisir un élève dans la classe
+/*	struct Eleve *premier; // Appel de la structure Eleve pour saisir un élève dans la classe
+	struct Classe *suivant; // Utilisation de liste chainée pour avoir une liste de classe */
 
-	struct Classe *suivant; // Utilisation de liste chainée pour avoir une liste de classe
+	struct Eleve TabEleve[MAXELEVE];
 } Classe_t;
 
-void SaisirClasse(Classe_t *C);
-void AfficherClasse(Classe_t C);
-
-#endif // CLASSE_H
+#endif //CLASSE_H
