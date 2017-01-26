48197434184cb932f072f77099efbcd613e66c33
Daffyneko <daffyneko@gmail.com>
2017-01-26 14:41:34 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef MENU_H
+#define MENU_H
+#include "ecole.h"
+
+void MenuEleve(Ecole_t E);
+void MenuClasse(Ecole_t E);
+void MenuEcole(Ecole_t E);
+
+#endif // MENU_H
+
