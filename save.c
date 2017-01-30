22fff0b82b6ae63da78afecf4876f5178cf4e746
delphinemaffre <delphine_maffre@hotmail.com>
2017-01-30 12:58:17 +0100

    Update save.c
    enfin une fonction Lire_Fichier_Classe qui marche!

--- save.c
+++ save.c
@@ -57,18 +57,15 @@ fgets(poubelle, sizeof(ligne), ptr_sauvegarde); // Pour sauter la première lign
 }
 
 
-void Lire_Fichier_Classe(const char *FichierEcole, Ecole_t *E, int nbClasse)
-
-
-{
-    int i,j;            // variable de boucle
-
-    FILE *ptr_fichier;  //pointeur sur fichier
-    char ligne[300];    //chaîne temporaire contenant les lignes du fichier (une a une)
+void Lire_Fichier_Classe(struct Ecole *E)
+{    int i, j, k;
+    char poubelle;
+    char test[2];
     char *champ;
-
-
-    ptr_fichier = fopen("FichierEcole.csv", "r"); // ouverture du fichier en mode lecture
+    char ligne[300];
+    FILE *ptr_fichier;
+//    printf("test 1");
+    ptr_fichier = fopen("FichierClasse6.csv", "r"); // ouverture du fichier en mode lecture
 
     if(!ptr_fichier) // si l'ouverture se passe mal
     {
@@ -76,55 +73,65 @@ void Lire_Fichier_Classe(const char *FichierEcole, Ecole_t *E, int nbClasse)
         exit(EXIT_FAILURE);
     }
 
-    while (!feof (ptr_fichier))
-    for(i=0; i<E->nbClasse; i++) // on parcourt le tableau de classe
-    {
-        fgets(ligne, 300, ptr_fichier); // lire la ligne
+
+    for (i=0; i<E->NbClasse; i++)
+  // fgets(poubelle, sizeof(ligne), ptr_fichier); // Pour sauter la première ligne qui est tout simplement le détail de ce que représente chaque colonnes
+ //  printf("test %d\n", i);
+
+   {    fgets(ligne, 300, ptr_fichier); // lire la ligne
 
         champ= strtok(ligne, ";"); // mettre dans champ les éléments de ligne jusqu'au prochain ';'
         strcpy(E->TabClasse[i].niveau, champ); // copier champ dans le niveau de la classe
+        printf("%s", champ);
 
         champ=strtok(NULL, ";"); // mettre dans champ les éléments de ligne jusqu'au prochain ';'
-       E->TabClasse[i].numClasse=atoi(champ);//transformer champ en nombre et le copier dans le numero de la classe
+        E->TabClasse[i].numClasse=atoi(champ);//transformer champ en nombre et le copier dans le numero de la classe
+        printf("%s", champ);
 
+        champ=strtok(NULL, ";");
+        strcpy(E->TabClasse[i].nomEnseignant, champ); // copier champ dans le niveau de la classe
+        printf("%s", champ);
 
         champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
         E->TabClasse[i].nbEleveClasse =atoi(champ); //transformer champ en nombre et le copier dans le nombre d'élèves dans la classe
+        printf("%s", champ);
 
-//printf("\n nb livres %d", tab[i].nb_livres);
-
-        for(j=0; j<E->TabClasse[i].nbEleveClasse; j++) // on parcourt les élèves de la classe
+       for (j=0; j<E->TabClasse[i].NbEleveClasse; j++)
         {
-            champ = strtok(NULL, ";");// mettre dans champ les élément de ligne jusqu'au prochain ';'
-            strcpy(E->TabClasse[i].TabEleve[j].nom, champ); //copier champ dans le nom de l'élèvé
-//        printf("\n  %s", champ);
-            champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-            strcpy(E->TabClasse[i].TabEleve[j].prenom, champ); //copier champ dans le prénom de l'élèvé
-//        printf("\n  %s", champ);
+       // printf("test %d\n", j);
+      fgets(ligne, 300, ptr_fichier); // lire la ligne
 
-            champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-           E->TabClasse[i].TabEleve[j].sexe = champ;//copier champ dans le sexe de l'élèvé
-//        printf("\n  %s", champ);
+        printf ("test3 \n");
+        champ = strtok(ligne, ";");// mettre dans champ les élément de ligne jusqu'au prochain ';'
+        printf ("test 4 \n");
 
-            champ = strtok(NULL, ";"); // mettre dans champ les élément de ligne jusqu'au prochain ';'
+        strcpy(E->TabClasse[i].TabEleve[j].nom, ligne); //copier champ dans le nom de l'élèvé
+        printf ("test 5\n");
+        printf("%s", champ);
+
+        champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
+        strcpy(E->TabClasse[i].TabEleve[j].prenom, champ); //copier champ dans le prénom de l'élèvé
+        printf("%s", champ);
+
+        champ=strtok(NULL, ";");
+        strcpy(test, champ);
+        E->TabClasse[i].TabEleve[j].sexe=test[0];
+        printf("%s", champ);
+
+        champ = strtok(NULL, ";"); // mettre dans champ les élément de ligne jusqu'au prochain ';'
             // copie les différents éléments du champ actuel, mais sans utiliser champ
 
-            memset (&E->TabClasse[i].TabEleve[j].dateNaissance, 0, sizeof(E->TabClasse[i].TabEleve[j].dateNaissance)); // initialisation de la structure tm (tmretour)
-            sscanf(champ,"%d/%d/%d", &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mday, &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon, &E->TabClasse[i].TabEleve[j].dateNaissance.tm_year);
-//            printf(" ------la date est %d/%d/%d----", &TabClasse[i].TabEleve[j].dateNaissance.tm_mday, &TabClasse[i].TabEleve[j].dateNaissance.tm_mday, &TabClasse[i].TabEleve[j].dateNaissance.tm_mday);
-            //soustraction de 1 mois et 1900 ans pour rentrer dans les conditions de tm (cf time.h)
-           E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon -= 1;
-           E->TabClasse[i].TabEleve[j].dateNaissance.tm_year -= 1900;
-
-
+        memset (&E->TabClasse[i].TabEleve[j].dateNaissance, 0, sizeof(E->TabClasse[i].TabEleve[j].dateNaissance)); // initialisation de la structure tm (tmretour)
+        sscanf(champ,"%d/%d/%d", &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mday,
+        &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon, &E->TabClasse[i].TabEleve[j].dateNaissance.tm_year);
+        printf("%s", champ);
+        /*    printf(" ------la date est %d/%d/%d----", E->TabClasse[i].TabEleve[j].dateNaissance.tm_mday,
+                   E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon, E->TabClasse[i].TabEleve[j].dateNaissance.tm_year);*/
 
         }
-
-
-
     }
-    fclose(ptr_fichier);
 }
+
 
 /*Ecrit un tableau de classe dans un fichier*/
 void Ecrire_Fichier_Classe(const char *FichierEcole2, Ecole_t E, int nbClasse)
@@ -195,54 +202,3 @@ fgets(poubelle, sizeof(ligne), ptr_sauvegarde); // Pour sauter la première lign
 }
 
 
-void test(struct Ecole *E)
-{
-    int i, j;
-    char *champ;
-    char ligne[300];
-    FILE *ptr_fichier;
-
-    ptr_fichier = fopen("Test2.csv", "r"); // ouverture du fichier en mode lecture
-
-    if(!ptr_fichier) // si l'ouverture se passe mal
-    {
-        perror("erreur ouverture du fichier");
-        exit(EXIT_FAILURE);
-    }
-
-    for (i=0; i<2; i++)
-        for (j=0; j<4; j++)
-        {
-printf("test %d\n", j);
-fgets(ligne, 300, ptr_fichier); // lire la ligne
-//printf("%s", ligne);
-  champ = strtok(ligne, ";");// mettre dans champ les élément de ligne jusqu'au prochain ';'
-
-            strcpy(E->TabClasse[i].TabEleve[j].nom, ligne); //copier champ dans le nom de l'élèvé
-
-            champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-
-            strcpy(E->TabClasse[i].TabEleve[j].prenom, champ); //copier champ dans le prénom de l'élèvé
-
-         //   champ = strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-         //   strcpy(E->TabClasse[i].TabEleve[j].sexe,champ);//copier champ dans le sexe de l'élèvé
-//        printf("\n  %s", champ);
-
-            champ = strtok(NULL, ";"); // mettre dans champ les élément de ligne jusqu'au prochain ';'
-            // copie les différents éléments du champ actuel, mais sans utiliser champ
-
-            memset (&E->TabClasse[i].TabEleve[j].dateNaissance, 0, sizeof(E->TabClasse[i].TabEleve[j].dateNaissance)); // initialisation de la structure tm (tmretour)
-            sscanf(champ,"%d/%d/%d", &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mday,
-                   &E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon, &E->TabClasse[i].TabEleve[j].dateNaissance.tm_year);
-
-        /*    printf(" ------la date est %d/%d/%d----", E->TabClasse[i].TabEleve[j].dateNaissance.tm_mday,
-                   E->TabClasse[i].TabEleve[j].dateNaissance.tm_mon, E->TabClasse[i].TabEleve[j].dateNaissance.tm_year);*/
-
-        }
-}
-
-
-for (i=0; i<4; i++)
-    printf("%s - %s - %d/%d/%d\n",E.TabClasse[0].TabEleve[i].nom, E.TabClasse[0].TabEleve[i].prenom,
-               E.TabClasse[0].TabEleve[i].dateNaissance.tm_mday, E.TabClasse[0].TabEleve[i].dateNaissance.tm_mon,
-               E.TabClasse[0].TabEleve[i].dateNaissance.tm_year);
