b5157ac9cd52dd584291ddb1de7078761b02dac0
delphinemaffre <delphine_maffre@hotmail.com>
2017-01-30 13:02:29 +0100

    Update classe.c
    déplacement de la fonction Ecrire_Fichier_Classe dans save.c

--- classe.c
+++ classe.c
@@ -71,37 +71,6 @@ void ModifierClasse(Classe_t *C)
     }
 }
 
-/*Ecrit un tableau de classe dans un fichier*/
-void Ecrire_Fichier_Classe(const char *FichierEcole, Classe_t *TabClasse, int nb)
-{ int i,j;   // variables de boucle
-
-    FILE *ptr_fichier; // pointeur de fichier
-
-    ptr_fichier =fopen(FichierEcole, "w"); // ouverture du fichier en mode écriture
-
-    if(!ptr_fichier) // si l'ouverture se passe mal
-    {
-        perror("erreur ouverture du fichier");
-        exit(EXIT_FAILURE);
-    }
-
-    for(i=0; i<nb; i++) // on parcourt le tableau de classes
-    {
-        // écriture du niveau de la classe, du numéro, du nom de l'enseignant et du nombre d'élèves
-        fprintf(ptr_fichier, "%s ; %d ; %s ;", TabClasse[i].niveau,TabClasse[i].numClasse, TabClasse[i].nomEnseignant);
-
-        for(j=0; j<TabClasse[i].nbEleveClasse; j++) // pour chaque classe on parcourt ses élèves
-        {
-          // écriture du prénom, nom, sexe et date de naissance
-            fprintf(ptr_fichier, "%s ; %s ; %c ; %d/%d/%d", TabClasse[i].TabEleve[j].nom,TabClasse[i].TabEleve[j].prenom, TabClasse[i].TabEleve[j].sexe, 
-            TabClasse[i].TabEleve[j].dateNaissance.tm_mday,TabClasse[i].TabEleve[j].dateNaissance.tm_mon, TabClasse[i].TabEleve[j].dateNaissance.tm_year);
-
-
-        }
-        fprintf(ptr_fichier,"\n"); // passage à la ligne pour inscrire la classe suivante (dans le tableau en paramètre)
-    }
-    fclose(ptr_fichier); // fermeture du fichier
-};
 
 
 /* LISTES CHAINEES-----------------------*********************
