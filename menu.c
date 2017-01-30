6d751e85d9492ede42bc68c667f0b1971f7e98e9
Daffyneko <daffyneko@gmail.com>
2017-01-30 13:54:11 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -3,15 +3,16 @@
 
 #include "menu.h"
 
-/***********************************************Daphné***********************************************/
+/***********************************************Daphne***********************************************/
 
 void MenuEleve(Ecole_t *E){
     int choix=-1;
     int i;
+    char nvelleclasse;
     Eleve_t eleve;
     do{
-        printf("\n*****Gestion des élèves*****\n");
-        printf("\nQue voulez-vous faire ?\n1 - Ajouter un élève\n2 - Afficher les élèves\n3 - Modifier un élève\n4 - Supprimer un élève\n0 - Retourner au menu précédent\n");
+        printf("\n*****Gestion des eleves*****\n");
+        printf("\nQue voulez-vous faire ?\n1 - Ajouter un eleve\n2 - Afficher les eleves\n3 - Modifier un eleve\n4 - Supprimer un eleve\n0 - Retourner au menu precedent\n");
         scanf("%d",&choix);
         getchar();
         switch(choix){
@@ -22,31 +23,33 @@ void MenuEleve(Ecole_t *E){
                 E->nbEleveTotal++;
                 printf("\n\nVous avez saisi les informations suivantes : ");
                 AfficherEleve(eleve);
-                printf("\n***Veuillez indiquer la classe à laquelle l'élève sera affecté.***\n");
+                printf("\n***Veuillez indiquer la classe a laquelle l'eleve sera affecte.***\n");
                 i=RechercherClasse(*E);
                 if(E->TabClasse[i].nbEleveClasse<MAXELEVE){
                         AffecterEleve(eleve, &E->TabClasse[i]);
                 }else{
-                        printf("\nAttention ! Cette classe est déjà complète. Voulez-vous créer une nouvelle classe ?(o/n) ");
+                        printf("\nAttention ! Cette classe est deja complete. Voulez-vous creer une nouvelle classe ?(o/n) ");
                         scanf("%c", &nvelleclasse);
                         getchar();
                         if(nvelleclasse=='o'){
                                 AjouterClasse(E, E->nbClasse);
                         }else{
-                                printf("\nOpération annulée.");
+                                printf("\nOperation annulee.");
                         }
                 }
+                RangerEcole(E);
                 break;
             case 2 :
-                printf("\n***Affichage des élèves***\n");
+                printf("\n***Affichage des eleves***\n");
                 AfficherEleveTotal(*E);
                 break;
             case 3 :
                 RechercherEleve(*E);
                 break;
             case 4 :
-                printf("\n***Suppression d'un élève***\n");
+                printf("\n***Suppression d'un eleve***\n");
                 SupprimerEleve(E);
+                RangerEcole(E);
                 break;
         }
     }while(choix!=0);
@@ -56,7 +59,7 @@ void MenuClasse(Ecole_t *E){
     int i;
     do{
         printf("\n*****Gestion des classes*****\n");
-        printf("\nQue voulez-vous faire ?\n1 - Ajouter une classe\n2 - Modifier une classe\n3 - Afficher une classe\n0 - Retourner au menu précédent\n");
+        printf("\nQue voulez-vous faire ?\n1 - Ajouter une classe\n2 - Modifier une classe\n3 - Afficher une classe\n0 - Retourner au menu precedent\n");
         scanf("%d",&choix);
         getchar();
         switch(choix){
@@ -65,6 +68,7 @@ void MenuClasse(Ecole_t *E){
             case 1 :
                 printf("\n\n*****Saisie d'une classe*****\n");
                 AjouterClasse(E, E->nbClasse);
+                RangerEcole(E);
                 break;
             case 2 :
                 //Modification d'une classe
@@ -81,19 +85,20 @@ void MenuClasse(Ecole_t *E){
 void MenuEcole(Ecole_t *E){
     int choix=-1;
     do{
-        printf("\n*****Gestion de l'école*****\n");
-        printf("\nQue voulez-vous faire ?\n1 - Saisir les informations de l'école\n2 - Modifier l'école\n3 - Afficher l'école\n0 - Retourner au menu précédent\n");
+        printf("\n*****Gestion de l'ecole*****\n");
+        printf("\nQue voulez-vous faire ?\n1 - Modifier les informations de l'ecole\n2 - Afficher l'ecole\n0 - Retourner au menu precedent\n");
         scanf("%d",&choix);
         getchar();
         switch(choix){
             default : break;
             case 0 : break;
             case 1 :
-                printf("\n\n*****Modification des informations de l'école*****\n");
+                printf("\n\n*****Modification des informations de l'ecole*****\n");
                 ModifierEcole(E);
                 break;
             case 2 :
-                printf("\n\n*****Affichage de l'école*****\n");
+                RangerEcole(E);
+                printf("\n\n*****Affichage de l'ecole*****\n");
                 AfficherEcole(*E);
                 break;
         }
