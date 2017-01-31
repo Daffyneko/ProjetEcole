39fd261fe020b50828a486175bc0975c0070b193
Daffyneko <daffyneko@gmail.com>
2017-01-31 13:57:14 +0100

    Update eleve.h

--- eleve.h
+++ eleve.h
@@ -4,17 +4,20 @@
 #ifndef ELEVE_H
 #define ELEVE_H
 
-#define MAX 30
-/***********************************************Daphné***********************************************/
+# define MAX 30
+# define CHAINE 150
+# define MAXELEVE 25
+
+/***********************************************Daphne***********************************************/
 typedef struct Eleve{
-	char nom[MAX]; // Nom de l'élève
-	char prenom[MAX]; // Prenom de l'élève
+	char nom[MAX]; // Nom de l'eleve
+	char prenom[MAX]; // Prenom de l'eleve
 	char sexe; // Sexe: juste un char pour mettre "m" ou "f"
 	struct tm dateNaissance; // Date de naissance. Utilisation de la structure tm. Ajout de la biblio <time.h>
 //	struct Parent renseignement; // Renseignement sur les parents numero nom et adresse
-//	struct Bulletin note; // Bulletins de notes. notes matières retard abscences...
+//	struct Bulletin note; // Bulletins de notes. notes matieres retard abscences...
 
-//	struct Eleve *suivant; // Utilisation de liste chainée pour avoir une liste d'élève
+//	struct Eleve *suivant; // Utilisation de liste chainee pour avoir une liste d'eleve
 
 } Eleve_t;
 
@@ -22,7 +25,5 @@ void SaisirEleve(Eleve_t *E);
 void AfficherEleve(Eleve_t E);
 void ModifierEleve(Eleve_t *E);
 void RangerEleves(Eleve_t E[], int nbe);
-
 /****************************************************************************************************/
-
 #endif // ELEVE_H
