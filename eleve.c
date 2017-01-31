5937f7c4f0f8197df9a2e6e0270ec72615184488
Daffyneko <daffyneko@gmail.com>
2017-01-31 10:41:30 +0100

    Update eleve.c

--- eleve.c
+++ eleve.c
@@ -4,79 +4,79 @@
 
 #include "eleve.h"
 
-/***********************************************Daphné***********************************************/
+/***********************************************Daphne***********************************************/
 
 void SaisirEleve(Eleve_t *E){
     int j, m, a;//variables de lecture de la date de naissance
     char sexe;
-    printf("\nVeuillez entrer le nom de l'élève : ");
+    printf("\nVeuillez entrer le nom de l'eleve : ");
     fscanf(stdin,"%s", E->nom);
-    printf("\nVeuillez entrer le prénom de l'élève : ");
+    printf("\nVeuillez entrer le prenom de l'eleve : ");
     fscanf(stdin,"%s", E->prenom);
     getchar();
     do{
-        printf("\nVeuillez entrer le sexe de l'élève (f pour fille ou g pour garçon) : ");
+        printf("\nVeuillez entrer le sexe de l'eleve (f pour fille ou g pour garçon) : ");
         fscanf(stdin,"%c", &sexe);
     }while(sexe!='g' && sexe!='f');
     E->sexe=sexe;
-    printf("\nVeuillez entrer la date de naissance de l'élève (jj/mm/aaaa) : ");
+    printf("\nVeuillez entrer la date de naissance de l'eleve (jj/mm/aaaa) : ");
     scanf("%d/%d/%d", &j, &m, &a);
     E->dateNaissance.tm_mday=j;
     E->dateNaissance.tm_mon=m-1;
     E->dateNaissance.tm_year=a-1900;
-    printf("\n***Elève enregistré***\n");
+    printf("\n***Eleve enregistre***\n");
 }
 void AfficherEleve(Eleve_t E){
     int longueur, i;
     longueur=strlen(E.nom);
-    printf("!%s",E.nom);
+    printf("|%s",E.nom);
     for(i=0;i<30-longueur;i++){
         printf(" ");
     }
     longueur=strlen(E.prenom);
-    printf("!%s",E.prenom);
+    printf("|%s",E.prenom);
     for(i=0;i<30-longueur;i++){
         printf(" ");
     }
-    printf("! %c  ",E.sexe);
-    printf("! %d/%d/%d       !\n", E.dateNaissance.tm_mday, E.dateNaissance.tm_mon+1, E.dateNaissance.tm_year+1900);
+    printf("| %c  ",E.sexe);
+    printf("| %d/%d/%d       |\n", E.dateNaissance.tm_mday, E.dateNaissance.tm_mon+1, E.dateNaissance.tm_year+1900);
 }
 void ModifierEleve(Eleve_t *E){
     int choix=-1;
     char sexe;
     int j, m, a;
-    printf("Veuillez saisir le champ à modifier : \n1 - Nom\n2 - Prénom\n3 - Sexe\n4 - Date de naissance\n0 - Annuler\n");
+    printf("Veuillez saisir le champ a modifier : \n1 - Nom\n2 - Prenom\n3 - Sexe\n4 - Date de naissance\n0 - Annuler\n");
     scanf("%d",&choix);
     switch(choix){
         default : break;
         case 0 :
-            printf("\n***Modification annulée***\n");
+            printf("\n***Modification annulee***\n");
             break;
         case 1 :
-            printf("\nVeuillez entrer le nom de l'élève : ");
+            printf("\nVeuillez entrer le nom de l'eleve : ");
             fscanf(stdin,"%s", E->nom);
-            printf("\n***Modification effectuée***\n");
+            printf("\n***Modification effectuee***\n");
             break;
         case 2 :
-            printf("\nVeuillez entrer le prénom de l'élève : ");
+            printf("\nVeuillez entrer le prenom de l'eleve : ");
             fscanf(stdin,"%s", E->prenom);
-            printf("\n***Modification effectuée***\n");
+            printf("\n***Modification effectuee***\n");
             break;
         case 3 :
             do{
-                printf("\nVeuillez entrer le sexe de l'élève (f pour fille ou g pour garçon) : ");
+                printf("\nVeuillez entrer le sexe de l'eleve (f pour fille ou g pour garçon) : ");
                 fscanf(stdin,"%c", &sexe);
             }while(sexe!='g' && sexe!='f');
             E->sexe=sexe;
-            printf("\n***Modification effectuée***\n");
+            printf("\n***Modification effectuee***\n");
             break;
         case 4 :
-            printf("\nVeuillez entrer la date de naissance de l'élève (jj/mm/aaaa) : ");
+            printf("\nVeuillez entrer la date de naissance de l'eleve (jj/mm/aaaa) : ");
             scanf("%d/%d/%d", &j, &m, &a);
             E->dateNaissance.tm_mday=j;
             E->dateNaissance.tm_mon=m-1;
             E->dateNaissance.tm_year=a-1900;
-            printf("\n***Modification effectuée***\n");
+            printf("\n***Modification effectuee***\n");
             break;
     }
 }
@@ -115,4 +115,5 @@ void RangerEleves(Eleve_t E[], int nbe){
     }
 
 }
+
 /****************************************************************************************************/
