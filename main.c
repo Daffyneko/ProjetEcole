a69af835ffaf094e8400710aee0f4a005ead757c
Tls-Cid <mermoud31@gmail.com>
2017-01-31 19:18:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
 /***********************************************Daphne***********************************************/
 int main(){
     Ecole_t MonEcole;
-    int choix=-1;
-
+    int choix=-1;/*
+LireEcole(&MonEcole);
+AfficherEcole(MonEcole);*/
     /* AUTHENTIFICATION
     char login[30]="admin";
     char mdp[30]="root";
@@ -30,32 +31,35 @@ int main(){
     LireEcole(&MonEcole);
     LireClasse(&MonEcole);
     Lire_Fichier_Classe(&MonEcole);
+
     printf("\n----------Bienvenue sur le logiciel de gestion de l'ecole----------\n");
     do{
-        printf("\nQue voulez-vous faire ?");
-        printf("\n1 - Tests unitaires");
-        printf("\n2 - Gestion de l'ecole");
-        printf("\n3 - Gestion des classes");
-        printf("\n4 - Gestion des eleves");
-        printf("\n0 - Quitter\n");
+        printf("\nQue voulez-vous faire ?\n");
+        printf("\n\t1 - Gestion de l'ecole");
+        printf("\n\t2 - Gestion des classes");
+        printf("\n\t3 - Gestion des eleves");
+        printf("\n\t4 - Backup");
+        printf("\n\t0 - Quitter\n");
         scanf("%d",&choix);
         getchar();
 
         switch(choix){
         default : return -1;
         case 0 : break;
-        case 1 ://Ecrivez ici les fonctions que vous voulez tester
+    /*    case 1 ://Ecrivez ici les fonctions que vous voulez tester
                 AfficherEleveTotal(MonEcole);
                 printf("\n\n%d",MonEcole.nbEleveTotal);
+                break;*/
+        case 3 :
+                MenuEleve(&MonEcole);
                 break;
         case 2 :
-                MenuEcole(&MonEcole);
-                break;
-        case 3 :
                 MenuClasse(&MonEcole);
                 break;
-        case 4 :
-                MenuEleve(&MonEcole);
+        case 1 :
+                MenuEcole(&MonEcole);
+                break;
+        case 4 : MenuBackup(&MonEcole);
                 break;
         }
     }while(choix!=0);
